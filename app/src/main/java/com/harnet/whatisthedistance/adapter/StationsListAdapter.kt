efa4d86a672e10b7ece5171ccc7cfd6432135487
Harnet69 <harnet69@gmail.com>
2021-01-19 10:00:43 +0100

    List of stations shows

--- app/src/main/java/com/harnet/whatisthedistance/adapter/StationsListAdapter.kt
+++ app/src/main/java/com/harnet/whatisthedistance/adapter/StationsListAdapter.kt
@@ -1,5 +1,43 @@
 package com.harnet.whatisthedistance.adapter
 
-class StationsListAdapter {
-    
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.harnet.whatisthedistance.R
+import com.harnet.whatisthedistance.databinding.ItemStationBinding
+import com.harnet.whatisthedistance.model.Station
+
+class StationsListAdapter(private val stationsList: ArrayList<Station>) :
+    RecyclerView.Adapter<StationsListAdapter.StationsListViewHolder>() {
+
+    //for updating information from a backend
+    fun updateStationsList(newStationsList: ArrayList<Station>) {
+        stationsList.clear()
+        stationsList.addAll(newStationsList)
+        //reset RecycleView and recreate a list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StationsListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        // elements of the list transforms into views. DataBinding approach
+        val view = DataBindingUtil.inflate<ItemStationBinding>(
+            inflater,
+            R.layout.item_station,
+            parent,
+            false
+        )
+        return StationsListAdapter.StationsListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return stationsList.size
+    }
+
+    class StationsListViewHolder(var view: ItemStationBinding) : RecyclerView.ViewHolder(view.root)
+
+    override fun onBindViewHolder(holder: StationsListViewHolder, position: Int) {
+        holder.view.station = stationsList[position]
+    }
 }
--- app/src/main/java/com/harnet/whatisthedistance/view/StationsListFragment.kt
+++ app/src/main/java/com/harnet/whatisthedistance/view/StationsListFragment.kt
@@ -8,12 +8,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.harnet.whatisthedistance.R
+import com.harnet.whatisthedistance.adapter.StationsListAdapter
+import com.harnet.whatisthedistance.model.Station
 import com.harnet.whatisthedistance.viewModel.StationsListViewModel
 import kotlinx.android.synthetic.main.stations_list_fragment.*
 
 class StationsListFragment : Fragment() {
     private lateinit var viewModel: StationsListViewModel
+
+    private lateinit var stationsListAdapter: StationsListAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,11 +29,19 @@ class StationsListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        stationsListAdapter = StationsListAdapter(arrayListOf())
+
         viewModel = ViewModelProvider(this).get(StationsListViewModel::class.java)
 
         viewModel.refreshFromAPI()
 
         observeViewModel()
+
+        stations_list_recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = stationsListAdapter
+        }
     }
 
     private fun observeViewModel(){
@@ -37,6 +50,7 @@ class StationsListFragment : Fragment() {
             stations_list_recyclerView.visibility = View.VISIBLE
             stationsList_progressBar.visibility = View.INVISIBLE
             Log.i("StationsList", "StationsList arrived: $stationsList")
+            stationsListAdapter.updateStationsList(stationsList as ArrayList<Station>)
         })
 
         viewModel.mStationsLoadError.observe(viewLifecycleOwner, Observer {
