b1fc383e68fff83ee93e41c9581cb6653da0e287
Harnet69 <harnet69@gmail.com>
2021-01-19 08:07:42 +0100

    Stations are collecting from API

--- app/src/main/java/com/harnet/whatisthedistance/model/StationsAPI.kt
+++ app/src/main/java/com/harnet/whatisthedistance/model/StationsAPI.kt
@@ -2,13 +2,17 @@ package com.harnet.whatisthedistance.model
 
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 
 interface StationsAPI {
     // whole URL is https://koleo.pl/api/v2/main/station_keywords
     // header X-KOLEO-Version: 1
-    //TODO here can be a problem with API, because of unused header
 
     // annotation used for knowing how this method can be used
+    @Headers(
+        "X-KOLEO-Version: 1"
+    )
     @GET("api/v2/main/stations")// !!!this is an end point, not all url
-    fun getStations(): Single<List<Station>>
+    fun getStations() : Single<List<Station>>
 }
