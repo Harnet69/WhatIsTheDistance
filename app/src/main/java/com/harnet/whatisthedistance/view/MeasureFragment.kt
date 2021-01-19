b18b96750d0b83e70d7a240792824f15ef04627c
Harnet69 <harnet69@gmail.com>
2021-01-19 12:51:44 +0100

    Created autofilling

--- app/src/main/java/com/harnet/whatisthedistance/view/MeasureFragment.kt
+++ app/src/main/java/com/harnet/whatisthedistance/view/MeasureFragment.kt
@@ -5,14 +5,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.harnet.whatisthedistance.R
+import com.harnet.whatisthedistance.model.StationKeyword
 import com.harnet.whatisthedistance.viewModel.MeasureViewModel
 import kotlinx.android.synthetic.main.measure_fragment.*
+
 
 class MeasureFragment : Fragment() {
     private lateinit var viewModel: MeasureViewModel
@@ -48,14 +52,30 @@ class MeasureFragment : Fragment() {
         viewModel.mStationsKeywords.observe(viewLifecycleOwner, Observer { stationsKeywordsList ->
             measure_progressBar.visibility = View.INVISIBLE
             Log.i("StationsList", "Stations keywords: ${stationsKeywordsList[0].keyword}")
+            search_block_measureFragment.visibility = View.VISIBLE
+            setToAutoComplete(dep_st, stationsKeywordsList)
+            setToAutoComplete(arr_st, stationsKeywordsList)
         })
 
         viewModel.mErrorMsg.observe(viewLifecycleOwner, Observer { e ->
-            if(e != null){
+            if (e != null) {
+                search_block_measureFragment.visibility = View.INVISIBLE
                 measure_progressBar.visibility = View.INVISIBLE
-                measure_error_msg.text = "Smth wrong $e"
                 measure_error_msg.visibility = View.VISIBLE
+                measure_error_msg.text = "Smth wrong $e"
             }
         })
+    }
+
+    private fun setToAutoComplete(autoCompleteTextView: AutoCompleteTextView, stationsKeywords: ArrayList<StationKeyword>){
+        val stationsKeywordsList = arrayListOf<String>()
+        for(station in stationsKeywords){
+            stationsKeywordsList.add(station.keyword)
+        }
+
+        val arrayAdapter = context?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, stationsKeywordsList) }
+
+        autoCompleteTextView.setAdapter(arrayAdapter)
+        autoCompleteTextView.threshold = 1
     }
 }
