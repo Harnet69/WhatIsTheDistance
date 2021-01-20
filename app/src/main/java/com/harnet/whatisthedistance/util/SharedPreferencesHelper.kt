54c0d06f2bf367ff4a4baf3620809a32c9cc8e3c
Harnet69 <harnet69@gmail.com>
2021-01-20 10:49:24 +0100

    Created shared preferences helper

---
+++ app/src/main/java/com/harnet/whatisthedistance/util/SharedPreferencesHelper.kt
@@ -0,0 +1,51 @@
+package com.harnet.whatisthedistance.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import androidx.core.content.edit
+
+class SharedPreferencesHelper {
+    companion object {
+        private const val UPD_TIME = "Update time"
+        private const val IS_ABOUT_SHOWED = "Is about showed"
+        private var prefs: SharedPreferences? = null
+
+        @Volatile
+        private var instance: SharedPreferencesHelper? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context): SharedPreferencesHelper = instance ?: synchronized(
+            LOCK
+        ) {
+            instance ?: buildHelper(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildHelper(context: Context): SharedPreferencesHelper {
+            prefs = PreferenceManager.getDefaultSharedPreferences(context)
+
+            return SharedPreferencesHelper()
+        }
+    }
+
+    // save current time to SharedPreferences
+    fun saveTimeOfUpd(time: Long) {
+        prefs?.edit(commit = true) {
+            putLong(UPD_TIME, time)
+        }
+    }
+
+    //getLastUpdateTime
+    fun getLastUpdateTime(): Long? {
+        return prefs?.getLong(UPD_TIME, 0)
+    }
+
+    // set is About app was showed
+    fun setIsAboutShowed(isShowed: Boolean){
+        prefs?.edit(commit = true) {
+            putBoolean(IS_ABOUT_SHOWED, isShowed)
+        }
+    }
+}
--- app/src/main/java/com/harnet/whatisthedistance/view/MainActivity.kt
+++ app/src/main/java/com/harnet/whatisthedistance/view/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.harnet.whatisthedistance.R
+import com.harnet.whatisthedistance.util.SharedPreferencesHelper
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationView: BottomNavigationView
